b1756ed0f98c224ac06462a6d4d9086f6969d88c
NOZi <madiyar1000@mail.ru>
2023-03-13 22:49:18 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const { token } = require('./config.json');
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', message => {
+  if (message.content === 'ping') {
+    message.reply('Pong(gg)!');
+  }
+  
+});
+
+client.login(token);
