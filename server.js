31b75d34fe693c06c416f357ffbdde16dcb5e46f
NOZi <madiyar1000@mail.ru>
2023-03-14 19:43:23 +0600

    First blood

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
-const { token } = require('./config.json');
+require('dotenv').config();
+const CLIENT_TOKEN = process.env.CLIENT_TOKEN;
+const client = new Discord.Client({
+  intents: [
+    Discord.GatewayIntentBits.Guilds,
+    Discord.GatewayIntentBits.MessageContent,
+    Discord.GatewayIntentBits.GuildMessages,
+    Discord.GatewayIntentBits.DirectMessages,
+    Discord.GatewayIntentBits.GuildVoiceStates,
+  ]
+});
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Logged in as ${client.user.tag}`);
 });
 
-client.on('message', message => {
-  if (message.content === 'ping') {
-    message.reply('Pong(gg)!');
+client.on('messageCreate', message => {
+  console.log(`Recieved message ${message.content}`);
+  if (message.content === '!ping') {
+    message.reply('Pong!');
   }
-  
 });
 
-client.login(token);
+
+client.login(CLIENT_TOKEN);
